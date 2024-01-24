95ad4ef69b4ad8720d25862252b25dda21f5e7ca
Bunyod <89516132+drbunyod@users.noreply.github.com>
2024-01-24 16:04:29 +0500

    Add exercise 8 solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return Math.pow(base, exp);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let fact = 1;
+
+  for (let i = 1; i <= n; i++) {
+    fact *= i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
