0ac3999e9d5fcb55d6dd21c6be92d166c4d02602
Bunyod <89516132+drbunyod@users.noreply.github.com>
2024-01-25 19:28:40 +0500

    Add exercise 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const personAge = 'yearOfDeath' in person
+                            ? person.yearOfDeath - person.yearOfBirth
+                            : (new Date()).getFullYear() - person.yearOfBirth;
 
+        const oldestAge = 'yearOfDeath' in oldest
+                            ? oldest.yearOfDeath - oldest.yearOfBirth
+                            : (new Date()).getFullYear() - oldest.yearOfBirth;
+
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
