bf3e917b321055307fbc7259809126086892af51
Bunyod <89516132+drbunyod@users.noreply.github.com>
2024-01-25 18:36:05 +0500

    Add exercise 10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
 
+    let n1 = 1;
+    let n2 = 1;
+
+    for (let i = 2; i <= n; i++) {
+        n1 = n1 + n2;
+        n2 = n1 - n2;
+        n1 = n1 - n2;
+        n2 = n1 + n2;
+    }
+
+    return n1;
 };
 
 // Do not edit below this line
