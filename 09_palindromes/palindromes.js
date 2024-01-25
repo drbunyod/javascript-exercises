04fb5b041e55417d3cdb5850edf5994060ec870d
Bunyod <89516132+drbunyod@users.noreply.github.com>
2024-01-25 18:02:14 +0500

    Add exercise 9 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    strReversed = str.split('').reverse().join('');
+    return str === strReversed;
 };
 
 // Do not edit below this line
